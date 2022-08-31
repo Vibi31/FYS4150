9e26ac5b194993d46096ccaf1118f24156cea05c
Vibishan <76438327+Vibi31@users.noreply.github.com>
2022-08-31 19:43:31 +0200

    Add files via upload

---
+++ test_learn.cpp
@@ -0,0 +1,21 @@
+// refreshing my c++ memory 
+
+using namespace std;
+
+#include <iostream>
+
+int main()
+{
+  // Print a message to screen
+  std::cout << "Hello, World!\n";
+
+  // Return 0 when done
+  return 0;
+}
+
+// to compile its                                       g++ -c filename.cpp
+//we get a filename.o object, whcih we can rename 
+// to make it into an object to execute                 g++ -o filename.exe  
+// we can use .exe or anything else 
+//they can be both combined to run both at once         g++ test_learn.cpp -o test_learn
+//to run the file we type                               ./filename.exe
